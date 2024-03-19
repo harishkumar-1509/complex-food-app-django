78652775beccf3ec7c68b18a229f00ee9ffafa25
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-03-19 19:44:11 +0530

    completed upto user registration

--- food_online/accounts/urls.py
+++ food_online/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("register-user",views.register_user,name="register-user")
+    path("register-user/",views.register_user,name="register-user")
 ]
--- food_online/accounts/views.py
+++ food_online/accounts/views.py
@@ -1,6 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 def register_user(request):
-    return HttpResponse("This is a test user reg form")
+    if request.methos == 'POST':
+        # print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            # Ready to be saved, but don't save the form
+            user = form.save(commit=False)
+            user.set_password(password)
+            user.role = User.CUSTOMER
+            form.save()
+            messages.success(request,"Your account has been registered successfully!")
+            # create the user using create user method
+            
+            # first_name = form.cleaned_data['first_name']
+            # last_name = form.cleaned_data['last_name']
+            # username = form.cleaned_data['username']
+            # email = form.cleaned_data['email']
+            # password = form.cleaned_data['password']
+            # user = User.objects.create(username=username, email=email, password=password,first_name=first_name, last_name=last_name)
+            # user.role = User.CUSTOMER
+            # user.save()
+            
+            return redirect('register-user')
+        else:
+            print("Invalid form")
+            print(form.errors)
+    else:
+        form = UserForm()
+    context = {
+        "form": form,
+    }
+    return render(request,'accounts/registerUser.html',context=context)
